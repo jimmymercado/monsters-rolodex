766ea8b212edb06fb79917240e9b14995ad96112
Jimmy Mercado <developer@jimmymercado.com>
2020-09-09 23:28:55 -0700

    Updates

---
+++ precache-manifest.2eb78bcbe2a67a4ecf446b6f59325c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7cf2d137a91fe3e8c5e858185cd2c3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b3a4fc8769ef22416898",
+    "url": "/monsters-rolodex/static/css/main.8f1c6c02.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a4fc8769ef22416898",
+    "url": "/monsters-rolodex/static/js/main.ab33848a.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
