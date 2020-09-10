9cdb1339573f8858009ca8fe25512ac73b101896
Jimmy Mercado <developer@jimmymercado.com>
2020-09-10 00:04:59 -0700

    adding files for github pages

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {Card} from '../card/card.component';
 import './card-list.style.css';
 
-export const CardList = props => {
+export const CardList = (props) => {
     return(
         <div className='card-list'>
             {props.monsters.map(monster => (
